6acda063bf8f538d5f0eb92551cad30619bd63fd
Jacqueline Pezan <71908081+jpezan@users.noreply.github.com>
2021-03-08 19:18:42 -0600

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,61 +1,68 @@
 #Import modules
+#Import modules
 import os
 import csv
 
 #Import CSV
-file = os.path.join('..', 'Resources', 'budget_data.csv')
+budget_csv = os.path.join('..', 'Resources', 'budget_data.csv')
+
 #Read csv
-with open(file, newline='') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter = ',')
-    print(csvreader)
-    header = next(csvreader)
+with open(budget_csv, newline='') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter =',')
+    header = next(csv_reader)
+
+    print(f"Header: {header}")
+
     #Initializing empty lists for csv values 
     month_count = []
     profit = []
     change_profit = []
-    #month_profit = []
-
-    print(f"Header: {header}")
 
                       
-    #Extracting month & profit data from csv
+    #Reading months data from csv
     for row in csvreader:
         month_count.append(row[0])
-        profit.append(int(row[1]))
-    for i in range(len(profit)-1):
-        change_profit.append(profit[i+1]-profit[i])
-                      
-#Evaluate the increase and decrease from the list made
+        profit.append(row[1]))
+ 
+    #Calculating profit  
+    for x in range(1, len(profit)):
+        change_profit.append((int(profit[x])) - int(profit[x-1])))
+    
+    #Average profit 
+    profit_avg = sum(change_profit) / len(change_profit)
+    #Total months
+    total_months = len(months)
+    
+    #Increase/Decrease Profit
 increase = max(change_profit)
 decrease = min(change_profit)
 
-#using the index, 
-month_increase = change_profit.index(max(change_profit))+1
-month_decrease = change_profit.index(min(change_profit))+1
-
+    #Results 
 
 print("Financial Analysis")
-print("------------------------")
-print(f"Total Months:{len(month_count)}")
-print(f"Total: ${sum(profit)}")
-print(f"Average Change: {round(sum(change_profit)/len(change_profit),2)}")
-print(f"Greatest Increase in Profits: {month_count[month_increase]} (${(str(increase))})")
-print(f"Greatest Decrease in Profits: {month_count[month_decrease]} (${(str(decrease))})")      
+print("--------------------------")
+print("Total Months: " + str(total_months))
+print("Total: " + "$" + str(sum(profit)))
+print("Average Change: " + "$" + str(profit_avg))
+print("Greatest Increase in Profits: " + str(months[profit_change.index(max(change_profit))+1]) + " " + "$" +str(increase))
+print("Greatest Decrease in Profits: " + str(months[profit_change.index(min(change_profit))+1]) + " " + "$" + str(decrease))     
 
 
+#Text file results
 output = output.txt
-with open(output,"w") as new:
-    new.write("Financial Analysis")
-    new.write("\n")
-    new.write("------------------------")
-    new.write("\n")
-    new.write(f"Total Months:{len(month_count)}")
-    new.write("\n")
-    new.write(f"Total: ${sum(profit)}")
-    new.write("\n")
-    new.write(f"Average Change: {round(sum(change_profit)/len(change_profit),2)}")
-    new.write("\n")
-    new.write(f"Greatest Increase in Profits: {month_count[month_increase]} (${(str(increase))})")
-    new.write("\n")
-    new.write(f"Greatest Decrease in Profits: {month_count[month_decrease]} (${(str(decrease))})")
-                    
+with open(output,"w") as text:
+    text.write("Financial Analysis")
+    text.write("\n")
+    text.write("------------------------")
+    text.write("\n")
+    text.write(f"Total Months: " + str(total_months)
+    text.write("\n")
+    text.write(f"Total: " + "$" + str(sum(profit)))
+    text.write("\n")
+    text.write("Average Change: " + "$" + str(profit_avg))
+    text.write("\n")
+    text.write("Greatest Increase in Profits: " + str(months[profit_change.index(max(change_profit))+1]) + " " + "$" +str(increase))
+    text.write("\n")
+    text.write("Greatest Decrease in Profits: " + str(months[profit_change.index(min(change_profit))+1]) + " " + "$" + str(decrease))
+    text.close()
+                
